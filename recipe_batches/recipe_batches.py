d15c916a630be0da91ff84b3426619b6aae5493c
cecil john tantay <cjbtantay@gmail.com>
2019-10-30 01:25:43 -0700

    finish recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,32 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    # if ingredients does not match recipe properties
+    batch = float("inf")
+    results = {}
+    # keep track of item's frequency, loop through dictionary and return lowest number
+    for item in recipe:
+        if item in ingredients:
+            while ingredients[item] >= recipe[item]:
+                if item not in results:
+                    results[item] = 1
+                else:
+                    results[item] += 1
+                ingredients[item] = ingredients[item] - recipe[item]
+        else:
+            return 0
+    for item in results:
+        if results[item] < batch:
+            batch = results[item]
+    return batch
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -6,6 +6,7 @@ import argparse
 def find_max_profit(prices):
     max_profit = float("-inf")
     for i in range(0, len(prices)):
+        # takes current index
         for j in range(i + 1, len(prices)):
             if prices[j] - prices[i] > max_profit:
                 max_profit = prices[j] - prices[i]
